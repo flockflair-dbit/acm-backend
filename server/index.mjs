12651068f0db8454dfdf4d066fe00c3ad6777fe1
Jayesh Wani <106749064+JayeshWani7@users.noreply.github.com>
2024-07-14 01:01:52 +0530

    database changes

--- server/index.mjs
+++ server/index.mjs
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import students from "./routes/students.mjs";
 import auth from "./routes/auth.mjs";
@@ -11,15 +10,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Define routes
 app.use("/api/students", students);
 app.use("/api/auth", auth);
 
-app.use((err, _req, res, next) => {
-    res.status(500).send("Uh oh! An unexpected error occured.")
-})
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error stack trace for debugging
+    res.status(500).send("Uh oh! An unexpected error occurred.");
+});
 
+// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
